fb1d988ad11687a9826597aea47b5f0d9681e7d2
PabloMatev <57010721+PabloMatev@users.noreply.github.com>
2021-05-28 11:58:07 +0200

    db  update

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(db);
+    console.log("MongoDB Connected ");
+  } catch (err) {
+    console.log("this is the error >>>");
+    console.error(err.message);
+    console.log("this is the error ^^^");
+    process.exit(1);
+  }
+};
+
+module.exports = connectDb;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const connectDb = require("./config/db");
 
 const app = express();
+
+//Connect Db
+connectDb();
+
+app.get("/", (req, res) => res.send("Api Running"));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.get("/", (req, res) => res.send("Api Running"));
