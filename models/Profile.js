754194d5d0894b06ac908a5a7c14551da4f5f890
PabloMatev <57010721+PabloMatev@users.noreply.github.com>
2021-06-04 13:31:42 +0200

    post and get route profile

---
+++ models/Profile.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+router.get("/", (req, res) => {
+  res.send("Profile route");
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,74 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const profile = require("../../models/profile");
+const { check, validationResult } = require("express-validator/check");
 
-// @route GET api/users
+// @route GET api/me
 //@desc Test route
 //@access Public
 
-router.get("/", (req, res) => {
-  res.send("Profile route");
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await profile
+      .findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.send(500).send("server Error");
+  }
 });
+// @route Post api/me
+//@desc Test route
+//@access Public
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is Required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",".map((skill) => skill.trim()));
+    }
+    console.log(skills);
+    res.send("hello");
+  }
+);
 
 module.exports = router;
