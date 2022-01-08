dea85c00f71ea2ba08966d31c98db00eb07541ba
PabloMatev <57010721+PabloMatev@users.noreply.github.com>
2022-01-08 21:39:18 +0100

    jwt implementation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -64,11 +64,66 @@ router.post(
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
     if (skills) {
-      profileFields.skills = skills.split(",".map((skill) => skill.trim()));
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
     }
-    console.log(skills);
-    res.send("hello");
+
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
+// @route Post api/profile
+//@desc get all profiles
+//@access Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+// @route Post api/profile/user/:user_id
+//@desc get profile by user ID
+//@access Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profiles = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) return res.status(400).json({ msg: "Profile not found" });
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("server Error");
+  }
+});
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -15,11 +15,12 @@ const User = require("../../models/User");
 router.post(
   "/",
   [
-    check("name", "Name is required. ").not().isEmpty(),
-    check("email", "Enter a valid email. ").isEmail(),
-    check("password", "Enter a password of minimum 6 characters. ").isLength({
-      min: 6,
-    }),
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -28,46 +29,34 @@ router.post(
     }
 
     const { name, email, password } = req.body;
-
     try {
       let user = await User.findOne({ email });
       if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
+        res.status(400).json({ errors: [{ message: "User already exists" }] });
       }
 
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
-      });
-
+      const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
       user = new User({ name, email, avatar, password });
 
       const salt = await bcrypt.genSalt(10);
-
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
 
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
+      const payload = { user: { id: user.id } };
       jwt.sign(
         payload,
         config.get("jwtSecret"),
-        { expiresIn: 3600000 },
+        { expiresIn: 36000 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
         }
       );
+      // res.status(200).json(user._id);
     } catch (err) {
       console.log(err.message);
-      res.status(500).send("Server error");
+      res.status(500).send("server error");
     }
   }
 );
